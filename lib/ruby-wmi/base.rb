2cb71be18c07547efee9fa43ebac51d6b9df0b0a
rogerdpack <rogerpack2005@gmail.com>
2009-05-18 10:02:21 -0600

    1.9 compat

--- lib/ruby-wmi/base.rb
+++ lib/ruby-wmi/base.rb
@@ -49,8 +49,8 @@ module WMI
           d.count # needed to check for errors.  Weird, but it works.
         rescue => error
           case error.to_s
-          when /Invalid class/i : raise InvalidClass
-          when /Invalid query/i : raise InvalidQuery
+          when /Invalid class/i then raise InvalidClass
+          when /Invalid query/i then raise InvalidQuery
           end
         end
         d.to_a
